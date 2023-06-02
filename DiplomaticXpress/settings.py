f434dba942ee50f3510fce92e02b777003c8d7ff
Shri <shrinidhi.hegde@equentis.com>
2023-06-02 23:04:20 +0530

    changed host

--- DiplomaticXpress/settings.py
+++ DiplomaticXpress/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': 'localhost',
+        'HOST': 'app-db',
         'PORT': '5432',
     }
 }
